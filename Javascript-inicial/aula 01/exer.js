81f10e353f841178988a0caa379aad61093d9b89
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-02-18 15:28:32 -0300

    Aula variáveis

---
+++ Javascript-inicial/aula 01/exer.js
@@ -0,0 +1,35 @@
+//Declara uma variavel com a sua idade
+const idade = 17
+//Declarar uma variavel com a sua comida preferida e atribuir um valor
+var comidapf = "Strogonoff"
+// Declarar uma variavel e nao dar valor pra ela, sendo undefined
+var qualquer
+// Declarar 5 variaveis diferentes sem valores
+var nada,nada2,nada3,nada4,nada5
+// Declarar uma variavel contendo uma string
+var amigo = "ninguem"
+// Declare uma varivel contendo um numero dentro de uma string
+var numero = 10
+// Declare uma variavel com seu nome, e outra com sobrenome, e concatene os dois, depois faça um template string dos 2
+var nome,sobrenome, tudo
+nome = "Daniel"
+sobrenome = "Palacio"
+tudo = nome + sobrenome
+console.log("Olá meu nome é " + tudo)
+// Coloque a seguinte frase em uma variavel: It´s time
+var ingles = "It´s time"
+// verifique o tipo de todas as variaveis criadas
+console.log(typeof idade, idade)
+console.log(typeof comidapf, comidapf)
+console.log(typeof qualquer, qualquer)
+console.log(typeof nada, nada)
+console.log(typeof nada2, nada2)
+console.log(typeof nada3, nada3)
+console.log(typeof nada4, nada4)
+console.log(typeof nada5, nada5)
+console.log(typeof amigo, amigo)
+console.log(typeof numero, numero)
+console.log(typeof nome, nome)
+console.log(typeof sobrenome, sobrenome)
+console.log(typeof tudo, tudo)
+console.log(typeof ingles, ingles)
--- Javascript-inicial/aula 01/script.js
+++ Javascript-inicial/aula 01/script.js
@@ -1,1 +1,36 @@
-alert("Cabelo tem 3 namoradas")
+// // console.log("Ola alunos")
+
+// Variaveis
+// Alocando uma informação no espaço da memória ram
+var nome = "lucas"
+//String
+
+let idade = 18
+//number
+
+const temcabelocomprido = true
+// boolean
+
+// console.log(typeof nome, nome)
+// console.log(typeof idade, idade)
+// console.log(typeof temcabelocomprido,temcabelocomprido)
+
+// console.log(typeof nome, typeof idade, typeof temcabelocomprido)
+
+
+var preco = 25
+var quantidade = 5
+var total = (preco * quantidade) - 10
+
+// console.log(total)
+
+// Concatenação
+var sobrenome, cidade, ano 
+sobrenome = "Pereira"
+cidade = "Curitiba"
+ano = 2025
+
+console.log(nome, sobrenome, cidade)
+console.log("Meu nome é " + nome + sobrenome + ", moro em " + cidade + ".")
+
+console.log(`Meu nome é ${nome} ${sobrenome}, moro em ${cidade}.`)
