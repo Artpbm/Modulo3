f3b4debeb038a48e92510189bfe7c29c679fb965
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-02-19 15:08:01 -0300

    CSS

--- Javascript-inicial/aula 02/mat.js
+++ Javascript-inicial/aula 02/mat.js
@@ -3,26 +3,33 @@ function somar(){
     var numero2 = parseFloat(document.getElementById("n2").value)
     var res = document.getElementById("res")
     res.innerHTML = numero1 + numero2
-    return res
+
    }   
    function subtracao(){
     var numero3 = parseFloat(document.getElementById("n3").value)
     var numero4 = parseFloat(document.getElementById("n4").value)
     var res = document.getElementById("res-")
     res.innerHTML = numero3 - numero4
-    return res
+
    }
    function mult(){
     var numero5 = parseFloat(document.getElementById("n5").value)
     var numero6 = parseFloat(document.getElementById("n6").value)
     var res = document.getElementById("res*")
-    res.innerHTML = numero5 * numero6
-    return res
+    res.innerHTML = (numero5 * numero6).toFixed(1)
+   
    }
    function div(){
     var numero7 = parseFloat(document.getElementById("n7").value)
     var numero8 = parseFloat(document.getElementById("n8").value)
     var res = document.getElementById("res/")
-    res.innerHTML = numero7 / numero8
-    return res
+
+    if(numero8 !== 0){
+        res.innerHTML = (numero7 / numero8).toFixed(1)
+    }
+    else{
+        res.innerHTML = "Não é permitido divisão por 0"
+        document.getElementById("n8").style.border = "2px solid red"
+    }
+  
    }
